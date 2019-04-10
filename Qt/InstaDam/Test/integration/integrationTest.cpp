662c9f8da9d6f74f85da64b952adcad672998515
Doug Friedel <friededn@yahoo.com>
2019-04-10 13:26:53 -0500

    adding drawing tests

--- Qt/InstaDam/Test/integration/integrationTest.cpp
+++ Qt/InstaDam/Test/integration/integrationTest.cpp
@@ -1,7 +1,10 @@
 #include "integrationTest.h"
 
+#include <QGraphicsSceneMouseEvent>
+
 void IntegrationTest::initTestCase() {
     idm = new InstaDam();
+    idm->runningLocally = true;
     idm->addLabelHash("Label1", Qt::red);
     idm->addLabelHash("Label2", Qt::blue);
     idm->addLabelHash("Label3", Qt::green);
@@ -13,6 +16,140 @@ void IntegrationTest::initTestCase() {
 
 void IntegrationTest::cleanupTestCase() {
     delete idm;
+}
+
+void IntegrationTest::makeMouseDownEvent(QPointF point, Qt::MouseButton button,
+                                         PhotoScene::viewerTypes type) {
+    QGraphicsSceneMouseEvent event(QEvent::GraphicsSceneMousePress);
+    PhotoScene* scene = (type == PhotoScene::PHOTO_VIEWER_TYPE ? idm->scene :
+                                                                 idm->maskScene);
+    event.setButton(button);
+    event.setButtons(button);
+    event.setScenePos(point);
+    event.setModifiers(Qt::NoModifier);
+    event.setWidget(dynamic_cast<QWidget*>(scene));
+    event.setButtonDownPos(button, point);
+    event.setButtonDownScenePos(button, point);
+    scene->mousePressEvent(&event);
+}
+
+void IntegrationTest::makeMouseMoveEvent(QPointF from, QPointF to,
+                                         Qt::MouseButton button,
+                                         PhotoScene::viewerTypes type) {
+    QGraphicsSceneMouseEvent event(QEvent::GraphicsSceneMouseMove);
+    PhotoScene* scene = (type == PhotoScene::PHOTO_VIEWER_TYPE ? idm->scene :
+                                                                 idm->maskScene);
+    event.setButton(button);
+    event.setButtons(button);
+    event.setScenePos(to);
+    event.setLastScenePos(from);
+    event.setModifiers(Qt::NoModifier);
+    event.setWidget(dynamic_cast<QWidget*>(scene));
+    event.setButtonDownPos(button, from);
+    event.setButtonDownScenePos(button, from);
+    scene->mouseMoveEvent(&event);
+}
+
+void IntegrationTest::makeMouseReleaseEvent(QPointF from, QPointF to,
+                                            Qt::MouseButton button,
+                                            PhotoScene::viewerTypes type) {
+    QGraphicsSceneMouseEvent event(QEvent::GraphicsSceneMouseRelease);
+    PhotoScene* scene = (type == PhotoScene::PHOTO_VIEWER_TYPE ? idm->scene :
+                                                                 idm->maskScene);
+    event.setButton(button);
+    event.setButtons(button);
+    event.setScenePos(to);
+    event.setModifiers(Qt::NoModifier);
+    event.setWidget(dynamic_cast<QWidget*>(scene));
+    event.setButtonDownPos(button, from);
+    event.setButtonDownScenePos(button, from);
+    scene->mouseReleaseEvent(&event);
+}
+
+void IntegrationTest::makeKeyPressEvent(Qt::Key key) {
+    QKeyEvent event = QKeyEvent(QEvent::KeyPress, key, Qt::NoModifier);
+    idm->scene->keyPressEvent(&event);
+}
+
+int IntegrationTest::findLabelIndex(const QString name) const {
+    for (int i = 0; i < idm->labelButtons.size(); i++) {
+        if (idm->labelButtons[i]->text() == name) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+SelectItem* IntegrationTest::getItem(int type, ushort num) {
+    //size_t start = 0;//lastIdx + 1;
+    ushort count = 0;
+    for (std::list<SelectItem*>::reverse_iterator it = idm->scene->currentItems.rbegin();
+         it != idm->scene->currentItems.rend(); ++it) {
+        if ((*it)->type() == type){
+            count++;
+            if (count == num)
+                return (*it);
+        }
+    }
+    return nullptr;
+}
+
+void IntegrationTest::testWriteAndReadLocal() {
+    idm->setImgInName("testImg1.jpg");
+    idm->on_actionOpen_File_triggered();
+
+    // draw a rectangle
+    clickLabel("Label2");
+    clickRectangleSelect();
+    draw(rect1, rect2);
+    QCOMPARE(getItem(SelectItem::Rectangle)->myRect, QRectF(rect1, rect2));
+
+    // draw an ellipse
+    clickLabel("Label1");
+    clickEllipseSelect();
+    draw(ellipse1, ellipse2);
+    QCOMPARE(getItem(SelectItem::Ellipse)->myRect, QRectF(ellipse1, ellipse2));
+
+    // resize the ellipse
+    draw(ellipseCorner, ellipseResize);
+    QCOMPARE(getItem(SelectItem::Ellipse)->myRect, QRectF(ellipse1, ellipse2 + eShift));
+
+    // undo the resize
+    idm->undoGroup->undo();
+    QCOMPARE(getItem(SelectItem::Ellipse)->myRect, QRectF(ellipse1, ellipse2));
+
+    // redo the resize
+    idm->undoGroup->redo();
+    QCOMPARE(getItem(SelectItem::Ellipse)->myRect, QRectF(ellipse1, ellipse2 + eShift));
+
+    // draw another ellipse
+    draw(ellipse3, ellipse4);
+    QCOMPARE(getItem(SelectItem::Ellipse, 2)->myRect, QRectF(ellipse4, ellipse3));
+
+    // move the ellipse
+    draw(inEllipse, ellipseMove);
+    QCOMPARE(getItem(SelectItem::Ellipse, 2)->myRect, QRectF(ellipse4 + ellipseShift, ellipse3 + ellipseShift));
+
+}
+
+void IntegrationTest::testAnnotateMove() {
+
+}
+
+void IntegrationTest::testOpenProjectAndAnnotate() {
+
+}
+
+void IntegrationTest::testSaveAndNext() {
+
+}
+
+void IntegrationTest::testExport() {
+
+}
+
+void IntegrationTest::testAsserts() {
+
 }
 
 QTEST_MAIN(IntegrationTest)
--- Qt/InstaDam/Test/integration/integrationTest.h
+++ Qt/InstaDam/Test/integration/integrationTest.h
@@ -2,6 +2,7 @@
 #define INTEGRATIONTEST_H
 #include <QtTest/QtTest>
 #include "../appTest/instadam.h"
+#include "../appTest/ui_instadam.h"
 
 class IntegrationTest: public QObject {
     Q_OBJECT
@@ -9,9 +10,86 @@ class IntegrationTest: public QObject {
 private slots:
     void initTestCase();
     void cleanupTestCase();
+    void testWriteAndReadLocal();
+    void testAnnotateMove();
+    void testOpenProjectAndAnnotate();
+    void testSaveAndNext();
+    void testExport();
+    void testAsserts();
 
 private:
     InstaDam *idm;
+    QPointF rect1 = QPointF(20., 20.);
+    QPointF rect2 = QPointF(30., 30.);
+    QPointF rectRotate1 = QPointF(25., 25.);
+    QPointF rectRotate2 = QPointF(22., 28.);
+
+    QPointF ellipse1 = QPointF(110., 100.);
+    QPointF ellipse2 = QPointF(145., 180.);
+    QPointF ellipseCorner = QPointF(144., 179.);
+    QPointF ellipseResize = QPointF(160., 150.);
+    QPointF eShift = ellipseResize - ellipseCorner;
+
+    QPointF ellipse3 = QPointF(100., 200.);
+    QPointF ellipse4 = QPointF(90., 10.);
+    QPointF inEllipse = QPointF(95., 50.);
+    QPointF ellipseMove = QPointF(200., 150.);
+    QPointF ellipseShift = ellipseMove - inEllipse;
+
+    QPointF poly1 = QPointF(300., 10.);
+    QPointF poly2 = QPointF(320., 15.);
+    QPointF poly3 = QPointF(400., 190.);
+    QPointF poly4 = QPointF(295., 156.);
+    QPointF poly5 = QPointF(298., 158.);
+    QPointF inPoly = QPointF(330., 100.);
+    QPointF movePoly = QPointF(325., 200.);
+    QPointF moveShift = movePoly - inPoly;
+    QPointF polyVertex = QPointF(401., 191.);
+    QPointF moveVertex = QPointF(365., 180.);
+    QPointF vertexShift = moveVertex - polyVertex;
+
+    QPointF free1 = QPointF(10., 10.);
+    QPointF free2 = QPointF(10., 10.);
+    QPointF free3 = QPointF(10., 10.);
+    QPointF free4 = QPointF(10., 10.);
+    QPointF free5 = QPointF(10., 10.);
+    QPointF free6 = QPointF(10., 10.);
+    QPointF erase1 = QPointF(10., 10.);
+    QPointF erase2 = QPointF(10., 10.);
+    QPointF erase3 = QPointF(10., 10.);
+
+    void makeMouseDownEvent(QPointF point, Qt::MouseButton button = Qt::LeftButton,
+                            PhotoScene::viewerTypes type = PhotoScene::PHOTO_VIEWER_TYPE);
+    void makeMouseMoveEvent(QPointF from, QPointF to, Qt::MouseButton button = Qt::LeftButton,
+                            PhotoScene::viewerTypes type = PhotoScene::PHOTO_VIEWER_TYPE);
+    void makeMouseReleaseEvent(QPointF from, QPointF to, Qt::MouseButton button = Qt::LeftButton,
+                               PhotoScene::viewerTypes type = PhotoScene::PHOTO_VIEWER_TYPE);
+    void makeKeyPressEvent(Qt::Key key);
+
+    int findLabelIndex(const QString name) const;
+
+    void clickLabel(int index) {idm->labelButtons[index]->click();}
+    void clickLabel(QString name) {idm->labelButtons[findLabelIndex(name)]->click();}
+    void clickRectangleSelect() {idm->ui->rectangleSelectButton->click();}
+    void clickEllipseSelect() {idm->ui->ellipseSelectButton->click();}
+    void clickPolygonSelect() {idm->ui->polygonSelectButton->click();}
+    void clickFreeDrawSelect() {idm->ui->freeSelectButton->click();}
+    void clickFinishPolygon() {idm->polygonSelectForm->finishPolygonButton->click();}
+    void clickInsertPoint() {idm->polygonSelectForm->insertPointButton->click();}
+    void clickRoundBrush() {idm->freeSelectForm->roundBrushButton->click();}
+    void clickSquareBrush() {idm->freeSelectForm->squareBrushButton->click();}
+    void clickDrawButton() {idm->freeSelectForm->drawButton->click();}
+    void clickEraseButton() {idm->freeSelectForm->eraseButton->click();}
+    void setBrushSize(int val) {idm->freeSelectForm->brushSizeSpinner->setValue(val);}
+    void clickMaskSelections() {idm->ui->showMaskSelections->click();}
+    void clickAddSelection() {idm->ui->addSelectionButton->click();}
+    void clickCancelSelection() {idm->ui->cancelSelectionButton->click();}
+    void draw(QPointF from, QPointF to) {
+        makeMouseDownEvent(from);
+        makeMouseMoveEvent(from, to);
+        makeMouseReleaseEvent(from, to);
+    }
+    SelectItem* getItem(int type, ushort num = 1);
 };
 
 #endif // INTEGRATIONTEST_H
